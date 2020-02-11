3b018e6cea889d07c237faf006a4ca56a30c5f3c
Erik Gúth <guthember@gmail.com>
2020-02-11 15:34:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Osztok
+{
+  class Program
+  {
+    static int OsztokSzama(int x)
+    {
+      int darab;
+
+      if (x == 0)
+      {
+        darab = 0;
+      }
+      else if (x == 1)
+      {
+        darab = 1;
+      }
+      else
+      {
+        darab = 2; // 1 és önmaga
+        for (int i = 2; i <= Math.Sqrt((double) x); i++)
+        {
+          if (x % i == 0)
+          {
+            darab++;
+          }
+        }
+      }
+
+      return darab;
+    }
+
+    static bool Prim(int x)
+    {
+      bool igaz = false;
+
+      if (x > 1)
+      {
+        if (OsztokSzama(x) == 2)
+        {
+          igaz = true;
+        }
+      }
+
+      return igaz;
+    }
+
+    static void Main(string[] args)
+    {
+      // Console.WriteLine(int.MaxValue);
+      Console.WriteLine("Kérek egy egész számot:");
+      int szam = Convert.ToInt32(Console.ReadLine());
+
+      #region Előző feladatok
+      //Console.WriteLine("A szám osztóinak száma: {0}",
+      //  OsztokSzama(szam));
+
+      //Console.WriteLine("Prímszámok: ");
+      //for (int i = 0; i <= 1000; i++)
+      //{
+      //  if (Prim(i))
+      //  {
+      //    Console.WriteLine(i);
+      //  }
+      //} 
+      #endregion
+
+      #region Hány darab prím
+      //int darab = 0;
+      //for (int i = 0; i <= szam; i++)
+      //{
+      //  if (Prim(i))
+      //  {
+      //    darab++;
+      //  }
+      //}
+      //Console.WriteLine("{0} darab prímszám van.",darab); 
+      #endregion
+
+      StreamWriter ki = new StreamWriter("primszamok.txt");
+
+      for (int i = 0; i <= szam; i++)
+      {
+        if (Prim(i))
+        {
+          ki.WriteLine(i);
+        }
+      }
+
+      ki.Close();
+      Console.WriteLine("Kiírás kész!");
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+﻿# Osztók számának megállapítása
+egész: darab 
+
+Ha x == 0 akkor
+	darab = 0
+Egyébként ha x == 1 akkor
+	darab = 1
+Egyébként
+    darab = 2 // 1 és önmaga miatt
+	Ciklus i:= 2-től x feléig
+		Ha x % i == 0 akkor
+			darab++
+		Feltétel vége
+	Ciklus vége
+Feltétel vége
+
+# Prímszám?
+Az OsztokSzama metódus segítségével
+döntjük el
+
+# Mennyi prím van?
+Bekérünk egy egész számot
+0-tól a bekért számig hány prímszám van.
+
+# Prímszámok fájlba írása
+Bekérünk egy egész számot
+0-tól a bekért számig fájlba írjuk a prímszámokat.
